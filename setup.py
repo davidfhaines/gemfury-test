6b40ac8efbaada76a1f1d844eaeef6b632f9f40e
David Haines <david@polyglotted.co>
2016-01-25 19:23:41 +0000

    gemfury packaging wip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gempypi',
+  packages = ['gempypi'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'A random test lib',
+  author = 'David Haines',
+  author_email = 'david@polyglotted.io',
+  url = 'https://github.com/davidfhaines/gempypi', # use the URL to the github repo
+  download_url = 'https://github.com/davidfhaines/gempypi/tarball/0.0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = []
+)
